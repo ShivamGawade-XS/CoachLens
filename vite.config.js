6d4f55b69a0549217d42df77eb476923b9adabf1
ShivamGawade-XS <24ec25@aitdgoa.edu.in>
2026-05-10 17:40:07 +0530

    build: configure manualChunks to optimize production build sizes

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'vendor-react': ['react', 'react-dom', 'react-router-dom'],
+          'vendor-charts': ['recharts'],
+          'vendor-utils': ['lucide-react', 'html2canvas', 'jspdf'],
+        }
+      }
+    }
+  }
 })
